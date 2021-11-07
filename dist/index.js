efdc86504231db21ddaf58911be3eb0f26697091
devzolo <zolotorevsky@gmail.com>
2021-11-07 03:54:00 -0300

    Updates

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.GLUT = void 0;
 const path_1 = __importDefault(require("path"));
 exports.GLUT = require(path_1.default.join(__dirname, '..', 'bin', process.platform, process.arch, 'native-glut.node'));
 exports.GLUT.mainLoop = function () {
