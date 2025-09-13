dabb048aa72b7f86c9267c45c3fbdbf0b97ef75b
devzolo <zolotorevsky@gmail.com>
2025-09-13 01:32:55 -0300

    Add config.h for FreeGLUT configuration and update .gitignore to exclude config.h

---
+++ deps/freeglut-3.2.1/config.h
@@ -0,0 +1,41 @@
+/* config.h - Generated configuration for FreeGLUT */
+
+/* X11 Extensions */
+#define HAVE_X11_EXTENSIONS_XF86VMODE_H 1
+#define HAVE_X11_EXTENSIONS_XRANDR_H 1
+#define HAVE_X11_EXTENSIONS_XINPUT2_H 1
+
+/* System headers */
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_STDBOOL_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_IOCTL_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_ERRNO_H 1
+
+/* USB HID support */
+#define HAVE_USBHID_H 1
+
+/* Functions */
+#define HAVE_GETTIMEOFDAY 1
+#define HAVE_VFPRINTF 1
+#define HAVE_DOPRNT 1
+
+/* XParseGeometry implementation */
+#define NEED_XPARSEGEOMETRY_IMPL 1
+
+/* Integer types */
+#define HAVE_STDINT_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_ULONG_LONG 1
+
+/* Warning and errors printed? */
+#define FREEGLUT_PRINT_WARNINGS 1
+#define FREEGLUT_PRINT_ERRORS 1
+
+/* Platform specific */
+#ifndef WIN32
+#define HAVE_CONFIG_H 1
+#endif
